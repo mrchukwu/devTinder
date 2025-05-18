e7edb2fbee2936eb460e09c69d9dfc9d8cdb9ba6
Kennedy Chukwu <mrchukwusinbox@gmail.com>
2025-05-18 17:37:05 +0100

    express routing stage one test

--- src/app.js
+++ src/app.js
@@ -2,9 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.send("Hello World from Express...");
+app.get("/user", (req, res) => {
+  res.send("Hello from user route");
 });
+
+app.post("/user", (req, res) => {
+  res.send("Data successfuly saved to the database")
+})
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted succesfully!")
+})
+
+app.use("/test", (req, res) => {
+  res.send("Hello from test route");
+});
+
+
 
 app.listen(3000, () => {
   console.log("Server successfully listening on port 3000");
