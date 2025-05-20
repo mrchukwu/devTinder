03bd35ec78c13cbf2a4d29385df64876a5e9ca62
Kennedy Chukwu <mrchukwusinbox@gmail.com>
2025-05-20 15:07:25 +0100

    database created, user shcema created, user model created, signup test api created

--- src/app.js
+++ src/app.js
@@ -1,13 +1,32 @@
 const express = require("express");
-const connectDB = require("./config/database")
+const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
 
-connectDB().then(() => {
-  console.log("Database connection established...");
-  
-  app.listen(3000, () => {
-    console.log("Server successfully listening on port 3000");
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "kennedy",
+    lastName: "Chukwu",
+    emailId: "kennedy@gmai.com",
+    password: "kennedy@123",
   });
-}).catch(err => {
-  console.error("Database cannot be connected!! ")
-})
+
+  try {
+    await user.save();
+    res.send("User Added successfully");
+  } catch (err) {
+    res.status(400).send("Error saving the user: " + err.message);
+  }
+});
+
+connectDB()
+  .then(() => {
+    console.log("Database connection established...");
+    app.listen(3000, () => {
+      console.log("Server successfully listening on port 3000");
+    });
+    console.log("App.listen() has been called...");
+  })
+  .catch((err) => {
+    console.error("Database cannot be connected!! ");
+  });
