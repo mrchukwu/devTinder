7a7f06606d5f744baf2e8ba4ee8c08dc8208f7d3
Kennedy Chukwu <mrchukwusinbox@gmail.com>
2025-05-23 01:24:50 +0100

    create and validate user schema

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const User = require("./models/user");
-const user = require("./models/user");
+// const user = require("./models/user");
 
 const app = express();
 
 app.use(express.json());
 
+// POST - signup user
+//runValidators - working on post/signup
 app.post("/signup", async (req, res) => {
   const user = new User(req.body);
 
@@ -17,10 +19,14 @@ app.post("/signup", async (req, res) => {
       message: "User Added successfully",
     });
   } catch (err) {
-    res.status(400).send("Error saving the user: " + err.message);
+    res.status(400).send({
+      status: "fail",
+      message: "Error saving the user: " + err.message,
+    });
   }
 });
 
+//GET - user by email
 app.get("/user", async (req, res) => {
   const userEmail = req.body.emailId;
   console.log(req.body.emailId);
@@ -45,6 +51,7 @@ app.get("/user", async (req, res) => {
   }
 });
 
+// GET - user by ID
 app.get("/user/:id", async (req, res) => {
   const userId = req.params.id;
 
@@ -69,6 +76,35 @@ app.get("/user/:id", async (req, res) => {
   }
 });
 
+// //GET - user by ID
+// app.get("/user", async (req, res) => {
+//   const userId = req.body.userId;
+//   console.log(userId)
+
+//   try{
+//     const user = await User.findById(userId);
+
+//     if(!user){
+//       return res.status(404)
+//       .send({
+//         status: "failed",
+//         message: "Not User"
+//       })
+//     }
+//     res.status(200).send({
+//       status: "success",
+//       user: user
+//     })
+//   }catch(err){
+//     res.status(404)
+//     .send({
+//       status: "fail",
+//       message: "Error getting user: "
+//     })
+//   }
+// })
+
+// GET - feeds
 app.get("/feeds", async (req, res) => {
   try {
     const userFeeds = await User.find({});
@@ -90,21 +126,70 @@ app.get("/feeds", async (req, res) => {
   }
 });
 
+// PATCH - update user by ID
+//runValidators - working on update by ID
 app.patch("/user", async (req, res) => {
   const userId = req.body.userId;
   const data = req.body;
 
   try {
-    await User.findByIdAndUpdate({_id: userId}, data);
+  await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+      runValidators: true,
+      new : true
+    });
+
+    if(!updatedUser){
+      return res.send("user not found")
+    }
     res.status(200).send({
       status: "success",
-      message: "User successfully updated"
-    })
+      message: "User successfully updated",
+    });
   } catch (err) {
-    res.status(404).send("Something went wrong");
+    console.log(err)
+    res.status(404).send("Something went wrong" + err.message);
   }
 });
 
+// PATCH - Update user by emailId
+//runValidators - working on update by emailId
+app.patch("/user", async (req, res) =>{
+  const userEmail = req.body.emailId;
+  const updateData = {...req.body};
+  delete updateData._id;
+  delete updateData.emailId;
+
+  console.log(updateData)
+
+  try{
+   await User.findOneAndUpdate({emailId: userEmail}, updateData,{
+      new: true,
+      runValidators: true,
+    });
+
+    if (!updatedUser) {
+      return res.status(404).send({
+        status: "fail",
+        message: "User not found with provided email"
+      });
+    }
+
+    res.status(200).send({
+      status: "success",
+      message: "User successfully updated",
+    })
+  }catch(err){
+
+    res.send({
+      status: "error",
+      message: "Error updating user",
+      error: err.message,
+    });
+  }
+});
+
+// DELETE - user by ID
 app.delete("/user", async (req, res) => {
   const userId = req.body.userId;
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,26 +1,78 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     firstName: {
-        type: String,
+      type: String,
+      required: true,
+      minLength: 4,
+      MaxLength: 50,
     },
     lastName: {
-        type: String
+      type: String,
+      MaxLength: 50,
     },
-    emailId: {
-        type: String
-    },
-    password: {
-        type: String
-    },
-    age: {
-        type: Number
-    },
-    gender: {
-        type: String
-    }
-})
 
+    emailId: {
+      type: String,
+      lowercase: true,
+      required: [true, "Email is required"],
+      validator: function (value) {
+        return value.includes('@');
+      },
+      message: 'Email must contain "@" symbol',
+      unique: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: [true, "passwoard is required"],
+      match: [
+        /^(?=.*[A-Z])(?=.*[^A-Za-z0-9]).{5,}$/,
+        'Password must contain at least one uppercase letter and one special character',
+      ],
+    },
+
+    age: {
+      type: Number,
+      required: true,
+      min: 18,
+      validate(value){
+        return value >= 18;
+      },
+      message: "User must be at least 18 years old."
+    },
+
+    gender: {
+      type: String,
+      lowercase: true,
+      validate(value) {
+        if (!["male", "female", "other"].includes(value)) {
+          throw new Error("Gender data is not valid");
+        }
+      },
+    },
+
+    photoUrl: {
+      type: String,
+      default:
+        "https://www.freepik.com/premium-vector/man-professional-business-casual-young-avatar-icon-illustration_270519234.htm",
+    },
+
+    about: {
+      type: String,
+      default: "Hi I am here to connect.",
+    },
+
+    skills: {
+      type: [String],
+      lowercase: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
 module.exports = mongoose.model("User", userSchema);
-
