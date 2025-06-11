2f39653d0cbfcd892a975a5f548cc51ab6145e9d
Kennedy Chukwu <mrchukwusinbox@gmail.com>
2025-06-11 09:46:59 +0100

    get all connections established api created

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,8 @@ const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest")
 
+const USER_SAFE_DATA = ["firstName", "lastName", "photoUrl", "age", "gender", "about", "skills" ];
+
 userRouter.get("/user/requests/recieved", userAuth, async (req, res) => {
     try{
         const loggedInUser = req.user;
@@ -10,7 +12,8 @@ userRouter.get("/user/requests/recieved", userAuth, async (req, res) => {
         const connectionRequests = await ConnectionRequest.find({
             toUserId: loggedInUser._id,
             status: "interested"
-        }).populate("fromUserId", ["firstName", "lastName", "photoUrl", "age", "gender", "about", "skills" ]);
+        }).populate("fromUserId", USER_SAFE_DATA);
+        // }).populate("fromUserId", "firstName lastName photoUrl age gender about skills"); 
 
         res.status(200).json({
             status: "success",
@@ -19,7 +22,46 @@ userRouter.get("/user/requests/recieved", userAuth, async (req, res) => {
     }catch(err){
         res.status(404).json("Error: " + err.message);
     }
-})
+});
 
+userRouter.get("/user/connections", userAuth, async(req, res) => {
+    try{
+
+        const loggedInUser = req.user;
+
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                {toUserId: loggedInUser._id, status: "accepted"},
+                {fromUserId: loggedInUser._id, status: "accepted"}
+            ]
+        })
+        .populate("fromUserId", USER_SAFE_DATA)
+        .populate("toUserId", USER_SAFE_DATA);
+
+
+        const connections = connectionRequests.map(connection => {
+            const fromUserId = connection.fromUserId;
+            const toUserId = connection.toUserId;
+
+            if(fromUserId._id.toString() !== loggedInUser._id.toString()){
+                return fromUserId;
+            }else if(toUserId._id.toString() !== loggedInUser._id.toString()){
+                return toUserId;
+            }
+        }).filter(user => user);
+
+        res.status(200).json({
+            status: "success",
+            message: "all connections established",
+            data: connections
+        })
+
+    }catch{
+        res.status(400).json({
+            status: "failed",
+            message: "Error: " + err.message
+        })
+    }
+})
 
 module.exports = userRouter;
