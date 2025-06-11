3957ee5ba73093593d260c76e8a0eee57a7a0874
Kennedy Chukwu <mrchukwusinbox@gmail.com>
2025-06-11 22:14:46 +0100

    create feeds api, and update every change and refactoring made to codes

--- apiList.md
+++ apiList.md
@@ -19,8 +19,8 @@
 - POST /request/review/rejected/:requestId -->
 
 ##  userRouter
-- GET /user/connections
 - GET /user/requests/recieved
+- GET /user/connections
 - GET /user/feed - Gets you the profile of other users on platform
 
 - STATUS: ignore, interested, accepted, rejected
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
+const User = require("../models/user")
 const ConnectionRequest = require("../models/connectionRequest")
 
 const USER_SAFE_DATA = ["firstName", "lastName", "photoUrl", "age", "gender", "about", "skills" ];
@@ -59,6 +60,42 @@ userRouter.get("/user/connections", userAuth, async(req, res) => {
     }catch{
         res.status(400).json({
             status: "failed",
+            message: "Error: " + err.message
+        })
+    }
+})
+
+userRouter.get("/user/feed", userAuth, async(req, res) => {
+    try{
+        const loggedInUser = req.user;
+
+        const connecionRequests = await ConnectionRequest.find({
+            $or: [
+                {fromUserId: loggedInUser._id},
+                {toUserId: loggedInUser._id}
+            ]
+        }).select("fromUserId toUserId")
+
+        const hideUserFromField = new Set();
+        connecionRequests.forEach(req => {
+            hideUserFromField.add(req.fromUserId._id);
+            hideUserFromField.add(req.toUserId._id)
+        });
+
+        const users = await User.find({
+            $and: [
+                {_id: {$nin: Array.from(hideUserFromField)}},
+                {_id: {$ne: loggedInUser._id}}
+            ]
+        }).select(USER_SAFE_DATA);
+
+        res.status(200).json({
+            status: "success",
+            data: users
+        })
+    }catch(err){
+        res.status(400).json({
+            status: "error",
             message: "Error: " + err.message
         })
     }
